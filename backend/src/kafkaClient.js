839baf54bb3aa40e0f68b10bf48b7f0012dfa7d0
Thabish <thabish@Thabishs-MacBook-Air.local>
2024-03-03 14:20:54 +0530

    correction imports
    Signed-off-by: Thabish <thabish@Thabishs-MacBook-Air.local>

--- backend/src/kafkaClient.js
+++
@@ -1,9 +0,0 @@
-import { Kafka } from "kafkajs";
-
-const HOST = process.env.HOST_IP || ip.address();
-const kafkaClient = new Kafka({
-  clientId: "realtime-dashboard",
-  brokers: [`${HOST}:9092`],
-});
-
-export default kafkaClient;
